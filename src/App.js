f47ab8079d57a57f67e7f4cf97b9cf042e30e8bc
Dimas <sidmandimas@gmail.com>
2024-06-16 06:17:07 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    // Проверяем, что Web App подключен
-    if (window.Telegram.WebApp) {
+    if (window.Telegram && window.Telegram.WebApp) {
       const tg = window.Telegram.WebApp;
       tg.ready();
     }
   }, []);
 
+  const handleClick = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const newMessage = { id: Date.now(), text: 'чабба', position: { left: x + 'px', top: y + 'px' } };
+    setMessages([...messages, newMessage]);
+    setTimeout(() => {
+      setMessages(messages => messages.filter(msg => msg.id !== newMessage.id));
+    }, 2000); // Удалить сообщение через 2 секунды
+  };
+
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick}>
       <header className="App-header">
-        <h1>мама ёж</h1>
+        <button className="animated-button">
+          Нажми меня
+        </button>
+        <div className="messages-container">
+          {messages.map(msg => (
+            <div key={msg.id} className="message" style={{ left: msg.position.left, top: msg.position.top }}>
+              {msg.text}
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
